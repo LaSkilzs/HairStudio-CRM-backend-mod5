6eafbb1cc0ce05342b7fbb70f3bfa49322498114
La Fountain <github email address>
2019-03-03 09:31:14 -0500

    Test controller appointment

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class AppointmentsController < ApplicationController
-      
+
       def index
         @appointments = Appointment.all
         render json: @appointments
